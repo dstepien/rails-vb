15f0af155bed914af1510b26ac6f5056d16c0d57
Dawid Stępień <kontakt@dawidstepien.pl>
2013-02-09 15:31:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vbook::Application.config.secret_token = '96c238776236bab364e2bf7b3dca709998868b9b4aba78decd9bce478aaca6047124ea1041adb593a8ce5244bc27741bade54e024a57963c02a0b92222a5b758'
