6a1848f373a05a0046cce9009a30ad6cac9536b8
Dawid Stępień <kontakt@dawidstepien.pl>
2013-02-10 15:56:01 +0100

    Improve nav_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
-  def nav_link(body, url)
-    content_tag(:li, :class => ('active' if current_page?(url))) do
+  def nav_link(body, url, active_for_subpages = true)
+    active = active_for_subpages ?
+      Rails.application.routes.recognize_path(url)[:controller] == params[:controller] :
+      current_page?(url)
+
+    content_tag(:li, :class => ('active' if active)) do
       link_to body, url
     end
   end
