5b6f00d2099d5a76f7cc36592fe945ccfe576b9f
Dawid Stępień <kontakt@dawidstepien.pl>
2013-02-11 21:31:58 +0100

    Add i18n support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_i18n_locale_from_params
   protect_from_forgery
+
+  protected
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.include?(params[:locale].to_sym)
+          I18n.locale = params[:locale]
+        else
+          redirect_to root_url
+        end
+      end
+    end
+    def default_url_options
+      { locale: I18n.locale }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Vbook::Application.routes.draw do
-  resources :books
 
-
-  get "home/index"
+  scope '(:locale)' do
+    resources :books
+    get 'home/index'
+    root :to => 'home#index'
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,7 +55,6 @@ Vbook::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
