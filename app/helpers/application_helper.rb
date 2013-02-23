7f1831ce876957f9a259943235854783450290d1
Dawid Stępień <kontakt@dawidstepien.pl>
2013-02-23 10:54:43 +0100

    Add books menu - closes #6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,6 @@
 module ApplicationHelper
-  def nav_link(body, url, active_for_subpages = true)
-    active = current_page?(url)
-
-    if active_for_subpages
-      controller = url.is_a?(Hash) ? url[:controller] :
-        Rails.application.routes.recognize_path(url)[:controller]
-      active = controller == params[:controller]
-    end
-
-    content_tag(:li, :class => ('active' if active)) do
+  def nav_link(body, url)
+    content_tag(:li, :class => ('active' if current_page?(url))) do
       link_to body, url
     end
   end
