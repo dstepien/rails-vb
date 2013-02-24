995a3736fae826f6047977df948ce7838f7cb4f7
Dawid Stępień <kontakt@dawidstepien.pl>
2013-02-24 14:43:32 +0100

    Add cover attachment with paperclip - closes #8

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ActiveRecord::Base
   attr_accessible :description, :isbn, :publishing_date, :title, :author, :pages, :cover
 
-  has_attached_file :cover, styles: { medium: '300x300>', thumb: '100x100>' },
+  has_attached_file :cover, styles: { medium: '300x300>', thumb: '60x60>' },
                     url: '/assets/books/:id/:style/:basename.:extension',
                     path: ':rails_root/public/assets/books/:id/:style/:basename.:extension',
-                    default_url: '/images/:style/missing.png'
+                    default_url: '/assets/books/:style/missing.gif'
 end
