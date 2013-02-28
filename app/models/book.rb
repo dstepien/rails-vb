db905834aa942341d47fa594fffd1aa121154531
Dawid Stępień <kontakt@dawidstepien.pl>
2013-02-28 16:24:48 +0100

    Book's form improvements

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ActiveRecord::Base
   attr_accessible :description, :isbn, :publishing_date, :title, :author, :pages, :cover
 
-  has_attached_file :cover, styles: { medium: '300x300>', thumb: '60x60>' },
-                    url: '/assets/books/:id/:style/:basename.:extension',
-                    path: ':rails_root/public/assets/books/:id/:style/:basename.:extension',
-                    default_url: '/assets/books/:style/missing.gif'
+  has_attached_file :cover, styles: { medium: '230x230>', edit: '165x165>', thumb: '60x60>' },
+                    url: '/images/books/:id/:style/:basename.:extension',
+                    path: ':rails_root/public/images/books/:id/:style/:basename.:extension',
+                    default_url: '/images/books/:style/missing.gif'
 end
